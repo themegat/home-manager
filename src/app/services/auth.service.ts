f66951d16ac7b87b2d19c05c7dd14ce5f660df17
themegat <tshepomotsoeneng0@gmail.com>
2019-08-13 11:46:47 +0200

    Enanched authentication UX

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,33 @@
+import { User } from './../model/user.model';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
-import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private isAuthenticated: boolean = false;
-  constructor(public firebaseAuth: AngularFireAuth) { }
+  private currentUser: User = null;
+
+  constructor(public firebaseAuth: AngularFireAuth) {
+    var me = this;
+    this.firebaseAuth.auth.setPersistence("local").then(rs => {
+    }).catch(err => {
+      console.log("Error setting auth persistance: ", err);
+    });
+
+    this.firebaseAuth.auth.onAuthStateChanged(function (user) {
+      if (user) {
+        me.currentUser = new User(user.uid, user.email, user.photoURL, user.displayName, user.refreshToken, user.metadata.lastSignInTime);
+      }
+    });
+  }
 
   authenticateUser(email: string, password: string): Promise<User> {
     return new Promise(async (resolve, reject) => {
       await this.firebaseAuth.auth.signInWithEmailAndPassword(email, password).then(result => {
-        const user = new User(result.user.uid, result.user.email, result.user.photoURL, result.user.displayName);
+        const user = new User(result.user.uid, result.user.email, result.user.photoURL,
+          result.user.displayName, result.user.refreshToken, result.user.metadata.lastSignInTime);
         this.isAuthenticated = true;
         resolve(user);
       }).catch(error => {
@@ -23,5 +38,15 @@ export class AuthService {
 
   isUserAuthenticated(): boolean {
     return this.isAuthenticated;
+  }
+
+  isUserStillValid(): boolean {
+    console.log("User Current: ", this.currentUser);
+    if (this.currentUser == null) {
+      return false;
+    } else {
+      this.isAuthenticated = true;
+      return true;
+    }
   }
 }
